8e6d5287be70b7e81045ee79fe9eacca2a902bac
Malcolm <sciand6@gmail.com>
2020-12-07 10:53:39 -0500

    Added user login.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Sign up
 router.post("/signup", (req, res) => {
@@ -38,13 +39,47 @@ router.post("/signup", (req, res) => {
             });
           })
           .catch((err) => {
-            res.status(400).json({ msg: err });
+            return res.status(400).json({ msg: err });
           });
       })
       .catch((err) => {
-        res.status(400).json({ msg: err });
+        return res.status(400).json({ msg: err });
       });
   });
+});
+
+// Login
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  // Input validation
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ msg: "Please provide an email and password." });
+  }
+
+  // Check if user exists
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (!savedUser) {
+        return res.json({ msg: "Invalid email or password." });
+      }
+
+      bcrypt.compare(password, savedUser.password).then((pwMatch) => {
+        if (pwMatch) {
+          // Generate a token based off user id
+          const token = jwt.sign({ _id: savedUser._id }, process.env.JWTSecret);
+          // Send token and user info as response
+          const { username, email } = savedUser;
+          res.json({ token, user: { username, email } });
+        } else {
+          return res.status(400).json({ msg: "Invalid email or password." });
+        }
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({ msg: err });
+    });
 });
 
 module.exports = router;
